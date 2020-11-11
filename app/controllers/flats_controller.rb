00f895412e278a7e7bcb86c9db87463265f39fd1
Joanna Chaillou <joanna.chaillou@edu.escp.eu>
2020-11-11 16:28:55 +0100

    Add picture url to Flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,38 @@
 class FlatsController < ApplicationController
+  before_action :find_flat, only: [:show, :edit, :update]
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
 end
